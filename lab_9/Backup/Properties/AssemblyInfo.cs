dfad8d4fb725e88ee01b22de6a865491db6f36d3
Верхоланцева Екатерина РИС-22-1б <88082592+verxek@users.noreply.github.com>
2024-09-18 19:05:43 +0300

    Add files via upload

---
+++ lab_9/Backup/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("lab_9.tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab_9.tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("68eb59de-fdf3-40ab-8df5-d3a9ec9240b7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ lab_9/Backup/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Lab_9;
+
+namespace lab_9.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CalculateDistance_StaticMethod_ReturnsCorrectDistance()
+        {
+            // Arrange
+            double x = 3.0;
+            double y = 4.0;
+
+            // Act
+            double distance = Point.CalculateDistanceStatic(x, y);
+
+            // Assert
+            Assert.AreEqual(5.0, distance);
+        }
+
+        [TestMethod]
+        public void CalculateDistance_InstanceMethod_ReturnsCorrectDistance()
+        {
+            // Arrange
+            Point point = new Point(3.0, 4.0);
+
+            // Act
+            double distance = point.CalculateDistance();
+
+            // Assert
+            Assert.AreEqual(5.0, distance, 0.001);
+        }
+
+        [TestMethod]
+        public void UnaryOperators_DecrementCoordinates_ReturnsCorrectResult()
+        {
+            // Arrange
+            Point point = new Point(3.0, 4.0);
+
+            // Act
+            Point result = --point;
+
+            // Assert
+            Assert.AreEqual(2.0, result.X, 0.001);
+            Assert.AreEqual(3.0, result.Y, 0.001);
+        }
+    }
+}
